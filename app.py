7d2f8794c33a9d2a35156152b75297832a59d48e
Cristovao-210 <57817099+Cristovao-210@users.noreply.github.com>
2025-11-19 22:05:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -157,9 +157,9 @@ def gerar_tabela_pss(nome_arq, arquivo_pdf):
 uploaded_file = st.file_uploader("Selecionar arquivo:")  
 if uploaded_file is not None:
   gerar_tabela_pss(uploaded_file.name, uploaded_file)
-  opcao = st.selectbox("Selecione a ação necessária", ["", "Gerar HTML"])
-  if opcao == "Gerar HTML":
-    baixar_arquivos(f'{uploaded_file.name}.html')
+ # opcao = st.selectbox("Selecione a ação necessária", ["", "Gerar HTML"])
+ # if opcao == "Gerar HTML":
+  baixar_arquivos(f'{uploaded_file.name}.html')
   
   
  
