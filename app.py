f5f4b0b1a702d5cfcb7caae69afb002e33b40e27
Cristovao-210 <57817099+Cristovao-210@users.noreply.github.com>
2025-11-19 21:10:20 -0300

    Update app.py
    Inserindo versão do GERADOR_TABELA_PSS (COLAB)

--- app.py
+++ app.py
@@ -61,20 +61,22 @@ def corpo_tabela(dados_pss, ano, nome_pdf):
     pss.write(f'''
                   <tr>
                     <td style="text-align: center; padding: 5px;">&nbsp;{ano}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[0][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[1][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[2][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[3][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[4][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[5][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[6][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[7][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[8][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[9][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[10][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[11][2]}&nbsp;</td>
-                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[12][2]}&nbsp;</td>
-                  </tr>''')
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[0][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[1][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[2][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[3][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[4][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[5][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[6][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[7][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[8][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[9][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[10][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[11][1]}&nbsp;</td>
+                    <td style="text-align: center; padding: 5px;">&nbsp;{dados_pss[12][1]}&nbsp;</td>
+                  </tr>
+                  
+                  ''')
 
 def fechamento_tabela(nome_pdf):
   with open(f"{nome_pdf}.html", "a") as pss:
@@ -120,34 +122,24 @@ def gerar_tabela_pss(nome_arq, arquivo_pdf):
   cabecalho_tabela(nome_arq[:-4])
   # percorrendo tabelas
   for ind, tab in enumerate(tabela):
+  # retirando coluna desnecessária
+  tabela[ind] = tabela[ind].drop(columns=["Unnamed: 1", "REMUN.INFORMADA", "REMUN.REAJUSTADA"])
 
-    # retirando coluna desnecessária
-    tabela[ind] = tabela[ind].drop(columns=["Unnamed: 0"])
+  # Mudando nome das colunas (CABEÇALHO DA TABELA)
+  tabela[ind].columns = ['MES', 'REMUN.CONSIDERADA']
 
-    # aqui seria bom colocar o procedimento de tirar a linha que está como cabeçalho e colocar dentro da tabela
-    tabela[ind].loc[len(tabela[ind])] = [list(tabela[ind])[0], list(tabela[ind])[1], list(tabela[ind])[2], list(tabela[ind])[3]]# transformando cabeçalho em uma linha da tabela
+  # dicionário para guardar os dados
+  dados_pss = []
 
-    # Mudando nome das colunas (CABEÇALHO DA TABELA)
-    tabela[ind].columns = ['MES', 'PSS APURADO', 'REMU', 'REMUNERACAO PSS']
+  # colocando as demais linhas dentro da lista
+  for row in range(13):
+    dados_pss.append([tabela[ind]['MES'][row].replace(":",""), tabela[ind]["REMUN.CONSIDERADA"][row]])
 
-    # retirando coluna desnecessária
-    tabela[ind] = tabela[ind].drop(columns=["REMU"])
+  # inserindo informações na tabela
+  corpo_tabela(dados_pss)
 
-    # dicionário para guardar os dados
-    dados_pss = []
-
-    # colocando o mes de janeiro na primeira linha
-    dados_pss.append([tabela[ind]['MES'][12].replace("_ ","").replace(" :","").replace(":",""), tabela[ind]["PSS APURADO"][12], tabela[ind]["REMUNERACAO PSS"][12]])
-    
-    # colocando as demais linhas dentro do dicionário
-    for row in range(12):
-        dados_pss.append([tabela[ind]['MES'][row].replace("_ ","").replace(" :","").replace(":",""), tabela[ind]["PSS APURADO"][row], tabela[ind]["REMUNERACAO PSS"][row]])
-
-    # inserindo informações na tabela
-    corpo_tabela(dados_pss, ano, nome_arq[:-4])
-
-    # incrementando o ano
-    ano = int(ano) + 1
+  # incrementando o ano
+  ano = int(ano) + 1
 
   # fechando a parte final do arquivo e fazendo download
   fechamento_tabela(nome_arq[:-4])
@@ -159,39 +151,4 @@ uploaded_file = st.file_uploader("Selecionar arquivo:")
 if uploaded_file is not None:
   gerar_tabela_pss(uploaded_file.name, uploaded_file)
   
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# nome_do_arquivo = nome_arq
-  # if not os.path.exists('./pss'):
-  #   try:
-  #     os.mkdir('./pss')
-  #     st.write("Arquivo criado com sucesso!")
-  #   except:
-  #     st.error("Não foi possível criar o diretório")
-  # # os.chdir('/pss') 
-  # # caminho = os.getcwd()
-  # # arquivo = f'{caminho}/{nome_do_arquivo}'
-  # arquivo = f'pss/{nome_do_arquivo}'
-  # #print(caminho)
-  # # capturando caminho do arquivo
-  # path = arquivo
-
-
-
-    
+ 
